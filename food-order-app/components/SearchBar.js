7ff9077dc989fd4f5b3428665854f35e09fe7205
Debasis Saikia <debasissaikia@Debasiss-MacBook-Air.local>
2022-04-19 22:34:34 +0530

    card rendering

--- food-order-app/components/SearchBar.js
+++ food-order-app/components/SearchBar.js
@@ -6,8 +6,49 @@ import AntDesign from "react-native-vector-icons/AntDesign";
 
 const SearchBar = () => {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View style={{ marginTop: 15, flexDirection: "row" }}>
+      <GooglePlacesAutocomplete
+        placeholder="Search "
+        styles={{
+          textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: "700",
+            marginTop: 7,
+          },
+          textInputContainer: {
+            backgroundColor: "#eee",
+            borderRadius: 50,
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
+          },
+        }}
+        renderLeftButton={() => (
+          <View style={{ marginLeft: 10 }}>
+            <Ionicons name="location-sharp" size={24} />
+          </View>
+        )}
+        renderRightButton={() => (
+          <View
+            style={{
+              flexDirection: "row",
+              marginRight: 8,
+              backgroundColor: "white",
+              padding: 9,
+              borderRadius: 30,
+              alignItems: "center",
+            }}
+          >
+            <AntDesign
+              name="clockcircle"
+              size={11}
+              style={{ marginRight: 6 }}
+            />
+            <Text>Search</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
--- food-order-app/screens/Home.js
+++ food-order-app/screens/Home.js
@@ -1,16 +1,28 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 import Header from "../components/Header";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
+import FoodItem from "../components/FoodItem";
 
 const Home = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#eee" }}>
-      <View style={{ backgroundColor: "#fff", paddingVertical: 13 }}>
+      <View
+        style={{
+          backgroundColor: "#fff",
+          paddingVertical: 13,
+          paddingHorizontal: 10,
+        }}
+      >
         <Header />
         <SearchBar />
       </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <FoodItem />
+      </ScrollView>
     </SafeAreaView>
   );
 };
