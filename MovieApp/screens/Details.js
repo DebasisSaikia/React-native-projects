58de893821fa4e281d675d91590dc1693feab10b
DebasisSaikia <debasishsaikia136@gmail.com>
2022-06-23 20:52:43 +0530

    title and genres

--- MovieApp/screens/Details.js
+++ MovieApp/screens/Details.js
@@ -1,4 +1,4 @@
-import { Text, ScrollView, StyleSheet, Image, Dimensions, ActivityIndicator } from 'react-native'
+import { Text, ScrollView, StyleSheet, Image, Dimensions, ActivityIndicator, View } from 'react-native'
 import React from 'react'
 import { getDetails } from '../services/services'
 
@@ -18,23 +18,62 @@ const Details = ({route,navigation}) => {
     },[movieId])
 
   return (
+
     <ScrollView>
-        {!loaded && <ActivityIndicator size={'large'} color={'blue'} />}
+    <View style={styles?.container}>
+    
+    {!loaded && <ActivityIndicator size={'large'} color={'blue'} />}
      {loaded && 
+     <>
      <Image 
      style={styles?.image} 
      source={
          {uri:`https://image.tmdb.org/t/p/w500`+details?.poster_path}} 
      resizeMode="cover"
       />
+      <Text style={styles?.title}>{details?.title}</Text>
+      <View>
+        {details?.genres && (
+            <View style={styles?.genres}>
+                {details?.genres?.map((genre,ind)=>{
+                    return(
+                        <Text style={styles?.genre} key={ind}>{genre?.name}</Text>
+                    )
+                })}
+            </View>
+        )}
+      </View>
+      </>
      } 
+    </View>
+
     </ScrollView>
   )
 }
 
 const styles=StyleSheet.create({
     image:{
-        height: dimension/2,
+        height: dimension/2.5,
+        width: '100%'
+    },
+    title:{
+        fontSize:24,
+        fontWeight:'bold',
+        marginTop:10,
+        marginBottom:10
+    },
+    container:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    genres:{
+        flexDirection:'row',
+        alignItems:'center',
+    },
+    genre:{
+        marginRight:10,
+        fontWeight:'700'
     }
 })
 
